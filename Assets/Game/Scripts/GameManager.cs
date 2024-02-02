25ad49d4ea564de72ee91226cb4bc250ec5907d5
Itt0ushura <balashov580@gmail.com>
2024-02-02 20:32:27 +0200

    Update GameManager.cs

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
 
     public List<Tile> _deletionGroup = new List<Tile>();
     public List<Tile> _checkedTiles = new List<Tile>();
+    List<Tile> _combinedList = new List<Tile>();
 
     private void Start()
     {
@@ -50,17 +51,19 @@ public class GameManager : MonoBehaviour
 
     private void SearchMethod(List<Tile> checkedTiles, List<Tile> deleteGroup)
     {
+        
         for (int i = 0; i < _tileGenerator.Board.GetLength(0); i++)
         {
             for (int j = 0; j < _tileGenerator.Board.GetLength(1); j++)
             {
                 Tile tile = _tileGenerator.Board[i, j].Tile;
                 Tile tilebelow;
+                Tile tileabove;
                 if (checkedTiles.Contains(tile)) { continue; }
 
                 checkedTiles.Add(tile);
 
-                if (i+1 < _tileGenerator.Board.GetLength(0))
+                if (i + 1 < _tileGenerator.Board.GetLength(0))
                 {
                     tilebelow = _tileGenerator.Board[i + 1, j].Tile;
                 }
@@ -69,19 +72,34 @@ public class GameManager : MonoBehaviour
                     tilebelow = null;
                 }
 
-                if (!checkedTiles.Contains(tilebelow))
+                if (i - 1 < _tileGenerator.Board.GetLength(0))
+                {
+                    tileabove = _tileGenerator.Board[i - 1, j].Tile;
+                }
+                else
+                {
+                    tileabove = null;
+                }
+                
+                if (checkedTiles.Contains(tilebelow) == false)
                 {
                     checkedTiles.Add(tilebelow);
                 }
-                if (tilebelow != null && tile._color == tilebelow._color)
+
+                if (checkedTiles.Contains(tileabove) == false)
                 {
+                    checkedTiles.Add(tileabove);
+                }
+
+                if (tile._color == tileabove._color && tile._color == tilebelow._color)
+                {
+                    deleteGroup.Add(tileabove);
                     deleteGroup.Add(tile);
                     deleteGroup.Add(tilebelow);
-                    Delete(checkedTiles, deleteGroup);
-                    StartCoroutine(WaitandSearch());
                 }
             }
         }
+        StartCoroutine(WaitandSearch());
     }
 
     private void Delete(List<Tile> checkedTiles, List<Tile> deleteGroup)
@@ -90,12 +108,13 @@ public class GameManager : MonoBehaviour
         {
             Destroy(x.gameObject);
         }
-        deleteGroup.Clear();
+        deleteGroup.RemoveAll(item => item == null);
         checkedTiles.RemoveAll(item => item == null);
     }
     private IEnumerator WaitandSearch()
     {
         yield return new WaitForSeconds(5);
         SearchMethod(_checkedTiles, _deletionGroup);
+        Delete(_checkedTiles, _deletionGroup);
     }
 }
